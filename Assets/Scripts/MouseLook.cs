0d887799dd7f2d0549206878bee7666c30d4ce09
Hrishikesh <hrishikeshpramodnair@gmail.com>
2021-12-20 01:04:51 +0530

    Added basic first person setup

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private float mouseX;
+    private float mouseY;
+    private float xRotation = 0f;
+
+    // Control the mouse speed
+    public float mouseSensitivity = 100.0f;
+    public Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hide the cursor and lock it to screen centre
+        Cursor.lockState = CursorLockMode.Locked;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Unity is working on a new input system so make necessary changes in those cases
+        /* Differences in frame rate should not affect the speed of rotation
+         so we multiply it with delta time which ensures the speed is same
+        irrespective of the speed of the Update() fn */
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        /*Instead of just using the Rotate() method we use xRotation,
+         perform the decrement and then set it using localRotation
+        to be able to apply the clamp. Otherwise we won't know when the 
+        limit has been reached.*/
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float x;
+    private float z;
+    private Vector3 move;
+    private Vector3 velocity;
+    bool isGrounded;
+
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    // Reference to ground check object
+    public Transform groundCheck;
+    // Radius of sphere used for check
+    public float groundDistance = 0.4f;
+    /* Control what objects the sphere should check for 
+     * (eg: Water meshes wont matter so we set the layer such that 
+     * water meshes aren't incluedd*/
+    public LayerMask groundMask;
+    public float jumpHeight = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+        /*If we had just created a new vector instead like:
+         move = new Vector3(x,0,z)
+        then the position would be global but we want it to be relative
+        so we get the player's right direction vector & forward direction vector
+        and mulitply it with the axis value*/
+
+        // Create a tiny physics sphere to check if the ground has been hit.
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // If it's on the ground reset the downward velocity
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2.0f;
+            /* Sometimes changing it to 0 wont guarantee that the palyer
+             is on the ground because the ground chaeck depends on the 
+            input radius and we can't always be precise so just reset the 
+            downward velocity to a small value but enough to force the player
+            to the ground*/ 
+
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            // the equn for jump vel. (physics bodies) is sqrt(h x -2 x g)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+            Debug.Log("Jump");
+        }
+
+        /*To calculate the effect of graviy we first calculate the velocity 
+         due to gravity and the subsequent distance travelled by the object.
+        This must be done only if the body is not on ground.
+        Otherwise the values keeps increasing and the jump times will gradually decrease*/
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
